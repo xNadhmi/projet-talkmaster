40aad4d3ce71ec036b6319940120751081194352
xNadhmi <80103454+xNadhmi@users.noreply.github.com>
2025-05-15 16:37:06 +0200

    [NK] Update api url

--- frontend/src/services/authService.jsx
+++ frontend/src/services/authService.jsx
@@ -4,7 +4,7 @@ const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function loginRequest(email, password) {
 	try {
-		const response = await axios.post(`${API_URL}/login`, { email, password });
+		const response = await axios.post(`${API_URL}/auth/login`, { email, password });
 		return response.data;
 	} catch (error) {
 		throw error.response?.data?.message || "Erreur serveur.";
@@ -13,7 +13,7 @@ export async function loginRequest(email, password) {
 
 export async function registerRequest(payload) {
 	try {
-		const response = await axios.post(`${API_URL}/register`, payload);
+		const response = await axios.post(`${API_URL}/auth/register`, payload);
 		return response.data;
 	} catch (error) {
 		throw error.response?.data?.message || "Erreur lors de l'inscription.";
